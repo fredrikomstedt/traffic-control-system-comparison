f3cab05b9f6bf1850d6165f1e917d0bbf52936e6
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2018-03-25 21:28:01 +0200

    Added a traffic analyzer script #5
    It can currently keep track of all vehicle waiting times. Also modified trivial algorithm to work with script.

---
+++ traffic_analyzer.py
@@ -0,0 +1,33 @@
+import os, sys
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("Please declare environment variable 'SUMO_HOME'")
+import traci
+
+vehicles_checked = {}
+
+def addWaitingTimes(edge):
+    #Get the vehicles from the last step on the lane
+    vehicles = traci.edge.getLastStepVehicleIDs(edge)
+    for vehicle in vehicles:
+        if not vehicle in vehicles_checked:
+            vehicles_checked[vehicle] = traci.vehicle.getAccumulatedWaitingTime(vehicle)
+
+def getAverageWaitingTime():
+    time_sum = 0
+    vehicle_amount = 0
+    for vehicle in vehicles_checked:
+        vehicle_amount += 1
+        time_sum += vehicles_checked[vehicle]
+    return float(time_sum)/vehicle_amount
+
+def getAverageSquaredWaitingTime():
+    time_sum = 0
+    vehicle_amount = 0
+    for vehicle in vehicles_checked:
+        vehicle_amount += 1
+        time = vehicles_checked[vehicle]
+        time_sum += time**2
+    return float(time_sum)/vehicle_amount
--- trivial_traffic_light.py
+++ trivial_traffic_light.py
@@ -6,6 +6,7 @@ else:
     sys.exit("Please declare environment variable 'SUMO_HOME'")
 from sumolib import checkBinary
 import traci
+import traffic_analyzer
 
 YELLOW_TIME = 3
 GREEN_TIME = 30
@@ -14,13 +15,24 @@ NS_YELLOW_STATE = "YYyrrrYYyrrr"
 WE_GREEN_STATE = "rrrGGgrrrGGg"
 WE_YELLOW_STATE = "rrrYYyrrrYYy"
 
+class WaitingTimeListener(traci.StepListener):
+    def step(self, t=0):
+        traffic_analyzer.addWaitingTimes("west_left")
+        traffic_analyzer.addWaitingTimes("north_up")
+        traffic_analyzer.addWaitingTimes("east_right")
+        traffic_analyzer.addWaitingTimes("south_down")
+
 def run_algorithm():
     green = 0
     yellow = 0
     west_east = True
     yellow_phase = False
+    steps = 0
+    listener = WaitingTimeListener()
+    traci.addStepListener(listener)
     while traci.simulation.getMinExpectedNumber() > 0:
         traci.simulationStep()
+        steps += 1
 
         #Increment time for the different phases
         if yellow_phase:
@@ -46,6 +58,8 @@ def run_algorithm():
                 else:
                     traci.trafficlight.setRedYellowGreenState("intersection", NS_YELLOW_STATE)
 
+    print("Average waiting time: " + str(traffic_analyzer.getAverageWaitingTime()))
+    print("Average squared waiting time: " + str(traffic_analyzer.getAverageSquaredWaitingTime()))
     traci.close()
     sys.stdout.flush()
 
@@ -54,6 +68,6 @@ if __name__ == '__main__':
     sumoBinary = checkBinary('sumo-gui')
 
     #Connect to SUMO via TraCI
-    traci.start([sumoBinary, "-c", "intersection.sumocfg"])
+    traci.start([sumoBinary, "-c", "intersection.sumocfg", "--waiting-time-memory", "1000"])
 
     run_algorithm()
