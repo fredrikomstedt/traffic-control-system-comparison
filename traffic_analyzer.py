c2871b431566fcf24adac8c24687ce761396d993
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2018-03-27 16:36:04 +0200

    Changed how time is calculated for deterministic algorithm #3
    It now calculate all lanes' time needed, and for left lanes accomodate for the vehicles coming from the opposite direction

--- traffic_analyzer.py
+++ traffic_analyzer.py
@@ -45,36 +45,34 @@ def getNumberOfVehiclesOnEdge(edge):
     vehicles = traci.edge.getLastStepVehicleIDs(edge)
     return len(vehicles)
 
-def getTimeNeededToEnterIntersectionOnEdge(edge):
-    #Get the vehicles from a road
-    vehicles = traci.edge.getLastStepVehicleIDs(edge)
-    if len(vehicles) == 0:
-        return 0
+def getVehiclesOnLanes(vehicles):
     vehiclesOnLanes = {}
 
     #Get the amount of vehicles on each lane
     for vehicle in vehicles:
         lane = traci.vehicle.getLaneIndex(vehicle)
         if not lane in vehiclesOnLanes:
-            vehiclesOnLanes[lane] = 0
-        vehiclesOnLanes[lane] += 1
+            vehiclesOnLanes[lane] = []
+        vehiclesOnLanes[lane].append(vehicle)
+    return vehiclesOnLanes
 
-    #Find lane with most vehicles
-    max_number_of_vehicles_on_lane = 0
-    max_lane = ""
-    for lane in vehiclesOnLanes:
-        if vehiclesOnLanes[lane] > max_number_of_vehicles_on_lane:
-            max_number_of_vehicles_on_lane = vehiclesOnLanes[lane]
-            max_lane = lane
+def getTimeNeededToEnterIntersectionOnEdge(edge):
+    #Get the vehicles from a road
+    vehicles = traci.edge.getLastStepVehicleIDs(edge)
+    if len(vehicles) == 0:
+        return 0
+    vehiclesOnLanes = getVehiclesOnLanes(vehicles)
 
     #Intersection position
     intersection_position = traci.junction.getPosition("intersection")
 
-    #Find largest distance from max lane
-    max_distance = 0
-    max_distance_vehicle = ""
-    for vehicle in vehicles:
-        if traci.vehicle.getLaneIndex(vehicle) is max_lane:
+    #Get the max time
+    max_time = 0
+    for lane in vehiclesOnLanes:
+        #Find largest distance from lane
+        max_distance = 0
+        max_distance_vehicle = ""
+        for vehicle in vehiclesOnLanes[lane]:
             position = traci.vehicle.getPosition(vehicle)
             distance = sqrt((intersection_position[0] - position[0])**2 +
                             (intersection_position[1] - position[1])**2)
@@ -82,18 +80,48 @@ def getTimeNeededToEnterIntersectionOnEdge(edge):
                 max_distance = distance
                 max_distance_vehicle = vehicle
 
-    #Calculate time
-    #Assume 0.5 seconds for each vehicle to get started
-    vehicle_startup_time = 0.5
-    time = vehicle_startup_time*max_number_of_vehicles_on_lane
+        #Calculate time
+        #Assume 0.5 seconds for each vehicle to get started
+        vehicle_startup_time = 0.5
+        time = vehicle_startup_time*len(vehiclesOnLanes[lane])
 
-    #PQ-formula to find time needed from acceleration and speed to get
-    #to the middle of the intersection.
-    acceleration = traci.vehicle.getAccel(max_distance_vehicle)/2 #Assume linear increase
-    velocity = traci.vehicle.getSpeed(max_distance_vehicle)
-    time += -velocity/acceleration + sqrt((velocity/acceleration)**2 +
-                                            2*max_distance/acceleration)
-    return time
+        #A delay occurs if the vehicles travel left (as they have to
+        #wait for cars from the opposite side of the intersection)
+        #This is assumed to be .1 second per vehicle from the opposite
+        #side
+        if traci.vehicle.getLaneIndex(max_distance_vehicle) == 1:
+            #determine edge:
+            opposite_edge = ""
+            if "west" in edge:
+                opposite_edge = "east_left"
+            elif "east" in edge:
+                opposite_edge = "west_right"
+            elif "north" in edge:
+                opposite_edge = "south_up"
+            else:
+                opposite_edge = "north_down"
+
+            #Get vehicles on lanes
+            opposite_vehicles = traci.edge.getLastStepVehicleIDs(opposite_edge)
+            opposite_vehiclesOnLanes = getVehiclesOnLanes(opposite_vehicles)
+
+            #Get amount of vehicles on conflicting lane
+            vehicle_amount_opposite_lane = 0
+            for lane in opposite_vehiclesOnLanes:
+                if traci.vehicle.getLaneIndex(opposite_vehiclesOnLanes[lane][0]) == 0:
+                    vehicle_amount_opposite_lane = len(opposite_vehiclesOnLanes[lane])
+                    break
+            time += vehicle_amount_opposite_lane*0.1
+
+        #PQ-formula to find time needed from acceleration and speed to get
+        #to the middle of the intersection.
+        acceleration = traci.vehicle.getAccel(max_distance_vehicle)/2 #Assume linear increase
+        velocity = traci.vehicle.getSpeed(max_distance_vehicle)
+        time += -velocity/acceleration + sqrt((velocity/acceleration)**2 +
+                                                2*max_distance/acceleration)
+        if time > max_time:
+            max_time = time
+    return max_time
 
 def getDensityAndTimeOnEdge(edge):
     return getNumberOfVehiclesOnEdge(edge), getTimeNeededToEnterIntersectionOnEdge(edge)
